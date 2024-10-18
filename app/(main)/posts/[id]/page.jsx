03a8e48489268defd5c7708263435eadb59efb97
TunahanOrak <164731717+TunahanOrak@users.noreply.github.com>
2024-10-18 18:02:12 +0300

    posts id kısmı oluşturuldu

---
+++ app/(main)/posts/[id]/page.jsx
@@ -0,0 +1,16 @@
+import { createClient } from "@/utils/supabase/server"
+import { notFound } from "next/navigation";
+
+export default async function PostDetailPage({ params }) {
+    const { data, error } = await supabase.form("posts")
+    .select()
+    .eq("id", params.id)
+    .single();
+
+    if(!data) return notFound();
+    return(
+        <div>
+            <h1>Yazı Başlığı</h1>
+        </div>
+    )
+}
